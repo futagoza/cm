3261651358bcea02a08396ccb0a98c315c34b141
futagoza <futagoza.ryuu@gmail.com>
2015-02-06 21:35:22 +0200

    license: added links to cm github page and a MIT page

--- src/copyright.js
+++ src/copyright.js
@@ -1,5 +1,8 @@
 /*
+  This package is part of the cm project: https://github.com/futagoza/cm
+
   Copyright (c) 2012-2015 Futago-za Ryuu, Erispa <futagoza.ryuu@gmail.com>
+  The MIT License, http://opensource.org/licenses/MIT
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
