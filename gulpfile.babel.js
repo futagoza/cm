70f974addfb6e43434acc5dd8663353f2b4f6338
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2018-04-24 18:50:46 +0100

    Use Gulp v4

---
+++ gulpfile.babel.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const src = require( "gulp" ).src;
+const series = require( "gulp" ).series;
+const task = require( "gulp" ).task;
+const eslint = require( "gulp-eslint" );
+const mocha = require( "gulp-mocha" );
+const pump = require( "pump" );
+
+// Run ESLint on all `.js` and `.ts` files used.
+task( "lint", () => pump(
+
+    src( [
+        "packages/**/*.js",
+        "gulpfile.babel.js"
+    ] ),
+    eslint( { dotfiles: true } ),
+    eslint.format(),
+    eslint.failAfterError()
+
+) );
+
+// Run tests.
+task( "test", () => pump(
+
+    src( "packages/**/*.{spec,test}.js", { read: false } ),
+    mocha()
+
+) );
+
+// Default task.
+task( "default", series( "lint", "test" ) );
