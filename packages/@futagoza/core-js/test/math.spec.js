3526d060d47dcaf08129edfd39ab599f12129fe9
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2018-08-08 18:18:42 +0100

    Implemented @futagoza/core-js/math.RAD_PER_DEG

--- packages/@futagoza/core-js/test/math.spec.js
+++ packages/@futagoza/core-js/test/math.spec.js
@@ -2,11 +2,12 @@
 
 const { expect } = require( "chai" );
 const DEG_PER_RAD = require( "../math.DEG_PER_RAD" );
+const RAD_PER_DEG = require( "../math.RAD_PER_DEG" );
 const ImportablePath = require( "../lib/ImportablePath" );
 
 describe( ImportablePath( "math.DEG_PER_RAD" ), () => {
 
-    it( "should equal ", () => {
+    it( "should approximately be 0.0174532925199432", () => {
 
         // DEG_PER_RAD === 0.017453292519943295
 
@@ -14,6 +15,19 @@ describe( ImportablePath( "math.DEG_PER_RAD" ), () => {
 
             .to.be.a( "number" )
             .closeTo( 0.0174532925199432, 0.01 );
+
+    } );
+
+} );
+
+describe( ImportablePath( "math.RAD_PER_DEG" ), () => {
+
+    it( "should equal 57.29577951308232", () => {
+
+        expect( RAD_PER_DEG )
+
+            .to.be.a( "number" )
+            .that.equals( 57.29577951308232 );
 
     } );
 
