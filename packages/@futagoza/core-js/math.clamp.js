afcc472fe932fd83b32b58f32722453d7c816b21
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2018-08-09 01:31:05 +0100

    core-js/math.clamp: fix jsdoc types

--- packages/@futagoza/core-js/math.clamp.js
+++ packages/@futagoza/core-js/math.clamp.js
@@ -9,10 +9,10 @@ const P = require( "./lib/ImportablePath" )( "math.clamp( value, lower, upper )"
  * 
  * @specification esnext (stage 1)
  * @since 1.0.0-alpha.0
- * @param {Array} value The current number
- * @param {Array} lower The minimum number
- * @param {Array} upper The maximum number
- * @returns {*} The right-most element
+ * @param {Number} value The current number
+ * @param {Number} lower The minimum number
+ * @param {Number} upper The maximum number
+ * @returns {Number} The clamed value.
  */
 function clamp( value, lower, upper ) {
 
